fb27bd44fe08f9096ca5e8967c311eec24656623
sinayra <nayra@hotmail.co.jp>
2019-12-18 21:35:04 -0300

    Create Task Controller

---
+++ app/controllers/api/v1/task_controller.rb
@@ -0,0 +1,40 @@
+class Api::V1::TaskController < ApplicationController
+  def index
+    task = Task.all.order(created_at: :desc)
+    render json: task
+  end
+
+  def create
+    task = Task.create!(task_params)
+    if task
+      render json: task
+    else
+      render json: task:errors
+    end
+  end
+
+  def show
+    if task
+      render json:task
+    else
+      render json: task.errors
+    end
+  end
+
+  def destroy
+    task&.destroy # safe navigation operator: avoid nil errors
+    render json: {message: 'Task deleted!'}
+  end
+
+  private
+  
+  def task_params #Strong parameters
+    params.permit(:name, :description, :date)
+  end
+
+  def task
+    # ||= => task = task || Task.find(params[:id])
+    # @ instance variable - not public
+    @task ||= Task.find(params[:id]) 
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      get 'task/index'
+      post 'task/create'
+      get 'task/show', to: 'task#show'
+      get 'task/destroy/:id', to: 'task#destroy'
+    end
+  end
   root to: 'static_pages#home'
+  get '/*path' => 'static_pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
