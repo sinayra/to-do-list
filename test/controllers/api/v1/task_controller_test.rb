83267fb9cf95e21ed8a0cb4a2692e3ff7ba1fe4c
sinayra <nayra@hotmail.co.jp>
2019-12-18 21:34:19 -0300

    Create Task Model

---
+++ test/controllers/api/v1/task_controller_test.rb
@@ -0,0 +1,24 @@
+require 'test_helper'
+
+class Api::V1::TaskControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get api_v1_task_index_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get api_v1_task_create_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get api_v1_task_show_url
+    assert_response :success
+  end
+
+  test "should get destroy" do
+    get api_v1_task_destroy_url
+    assert_response :success
+  end
+
+end
